b3a1f4cd50f718e93f85e9ffe620d6a01497fd57
iamharshil <harshilc71@gmail.com>
2023-03-18 02:17:19 +0530

    02

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, rerum. Earum porro delectus, dolorum
+            cumque atque repellendus ratione. Ducimus ea animi nihil delectus beatae debitis sunt asperiores maxime
+            adipisci exercitationem, sequi optio in aut quos voluptatibus blanditiis pariatur fuga expedita tempore odio
+            ex rerum? Unde accusantium modi consectetur inventore reprehenderit ratione dolorem aliquam perferendis,
+            distinctio explicabo saepe optio libero fugit necessitatibus vero possimus fugiat. Velit ratione nesciunt
+            tempora saepe, voluptatibus tempore, neque atque culpa accusantium animi hic itaque, deleniti adipisci
+            assumenda dolorem labore repellendus cupiditate qui numquam laboriosam deserunt? Molestias modi placeat
+            maxime veniam delectus quia fugiat. Ab, nemo praesentium.
+        </p>
+    </article>
+
+    <a href="/">Go back</a>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return ['foo' => 'bar'];
-// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+});
+
+Route::get("/post", function() {
+    return view("post");
 });
